d6583f52e501775efc3c8d5d81f5cc7f56f6cb1b
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2022-03-25 10:46:12 +0000

    fine 01.0

---
+++ first-routing-app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { SpotifyService } from './spotify.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  query: string; //se la variabile da errore andare in tsconfig.json e mettere strict : false
+  title = 'first-routed-app';
+  obsTrack: Observable<Object>;
+  results: any;
+  // faccio iniettare lo spotify service e faccio una ricerca
+  constructor(public spotify: SpotifyService) {
+   
+  }
+
+  submit(query:HTMLInputElement): void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsTrack = this.spotify.searchTrack(this.query);
+    this.obsTrack.subscribe((data) => this.results = data); 
+  }
+}
